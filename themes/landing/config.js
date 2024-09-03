087c7936252f7a66346684f0e29a0b30da19532e
netthepoints <netthepoints@gmail.com>
2024-09-03 16:12:16 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   
-  LANDING_HEADER_BUTTON_1_TITLE: 'Resources',
+  LANDING_HEADER_BUTTON_1_TITLE: 'Resource',
   LANDING_HEADER_BUTTON_1_URL: 'https://github.com/tangly1024/NotionNext',
 
   LANDING_HEADER_BUTTON_2_TITLE: 'Contact',
