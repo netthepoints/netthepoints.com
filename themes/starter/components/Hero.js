93f0ab743e9940f76191c2c844d9296f3bdf4ee2
netthepoints <netthepoints@gmail.com>
2024-11-12 15:25:31 +0800

    Update Hero.js

--- themes/starter/components/Hero.js
+++ themes/starter/components/Hero.js
@@ -1,68 +1,59 @@
-/* eslint-disable @next/next/no-img-element */
-import { siteConfig } from '@/lib/config'
 import CONFIG from '../config'
-/**
- * 英雄大图区块
- */
-export const Hero = () => {
-  return <>
-     {/* <!-- ====== Hero Section Start --> */}
-     <div
-      id="home"
-      className="relative overflow-hidden bg-primary pt-[120px] md:pt-[130px] lg:pt-[160px]"
-    >
-      <div className="container">
-        <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4">
-            <div
-              className="hero-content wow fadeInUp mx-auto max-w-[780px] text-center"
-              data-wow-delay=".2s"
-            >
-              {/* 主标题 */}
-              <h1
-                className="mb-6 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug lg:text-5xl lg:leading-[1.2]"
-              >
-                {siteConfig('STARTER_HERO_TITLE_1', null, CONFIG)}
-              </h1>
-              {/* 次标题 */}
-              <p
-                className="mx-auto mb-9 max-w-[600px] text-base font-medium text-white sm:text-lg sm:leading-[1.44]"
-              >
-                {siteConfig('STARTER_HERO_TITLE_2', null, CONFIG)}
-              </p>
-              {/* 按钮组 */}
-              <ul
-                className="mb-10 flex flex-wrap items-center justify-center gap-5"
-              >
-                {siteConfig('STARTER_HERO_BUTTON_1_TEXT', null, CONFIG) &&
-                <li>
-                  <a
-                    href={siteConfig('STARTER_HERO_BUTTON_1_URL', null, CONFIG)}
-                    className="inline-flex items-center justify-center rounded-md bg-white px-7 py-[14px] text-center text-base font-medium text-dark shadow-1 transition duration-300 ease-in-out hover:bg-gray-2 hover:text-body-color"
-                  >
-                     {siteConfig('STARTER_HERO_BUTTON_1_TEXT', null, CONFIG)}
-                  </a>
-                </li>
-                }
-                {siteConfig('STARTER_HERO_BUTTON_2_TEXT', null, CONFIG) &&
-                <li>
-                  <a
-                    href={siteConfig('STARTER_HERO_BUTTON_2_URL', null, CONFIG)}
-                    target="_blank"
-                    className="flex items-center rounded-md bg-white/[0.12] px-6 py-[14px] text-base font-medium text-white transition duration-300 ease-in-out hover:bg-white hover:text-dark" rel="noreferrer"
-                  >
-                    {siteConfig('STARTER_HERO_BUTTON_2_ICON', null, CONFIG) && <img className='mr-4' src={siteConfig('STARTER_HERO_BUTTON_2_ICON', null, CONFIG)}/>}
-                    {siteConfig('STARTER_HERO_BUTTON_2_TEXT', null, CONFIG)}
-                  </a>
-                </li>
-                }
-              </ul>
+import ModalVideo from './ModalVideo'
+import { siteConfig } from '@/lib/config'
 
+export default function Hero() {
+  return (
+        <section className="relative">
+
+            {/* Illustration behind hero content */}
+            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-0 pointer-events-none -z-1" aria-hidden="true">
+                <svg width="1360" height="578" viewBox="0 0 1360 578" xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="illustration-01">
+                            <stop stopColor="#FFF" offset="0%" />
+                            <stop stopColor="#EAEAEA" offset="77.402%" />
+                            <stop stopColor="#DFDFDF" offset="100%" />
+                        </linearGradient>
+                    </defs>
+                    <g fill="url(#illustration-01)" fillRule="evenodd">
+                        <circle cx="1232" cy="128" r="128" />
+                        <circle cx="155" cy="443" r="64" />
+                    </g>
+                </svg>
             </div>
-          </div>
 
-          {/* 产品预览图片 */}
-          <ModalVideo
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+
+                {/* Hero content */}
+                <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+                    {/* Section header */}
+                    <div className="text-center pb-12 md:pb-16">
+                        <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
+                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">{siteConfig('LANDING_HERO_TITLE_1', null, CONFIG)}</span>
+                        </h1>
+                        <div className="max-w-3xl mx-auto">
+                            <p className="text-xl text-gray-600 dark:text-gray-400 mb-8" data-aos="zoom-y-out" data-aos-delay="150">{siteConfig('LANDING_HERO_P_1', null, CONFIG)}</p>
+                            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
+                                <div>
+                                    <a target='_blank' className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
+                                        href={siteConfig('LANDING_HERO_BUTTON_1_LINK', null, CONFIG)} rel="noreferrer">
+                                        {siteConfig('LANDING_HERO_BUTTON_1_TEXT', null, CONFIG)}
+                                    </a>
+                                </div>
+                                <div>
+                                    <a target='_blank' className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
+                                        href={siteConfig('LANDING_HERO_BUTTON_2_LINK', null, CONFIG)} rel="noreferrer">
+                                        {siteConfig('LANDING_HERO_BUTTON_2_TEXT', null, CONFIG)}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Hero image */}
+                    <ModalVideo
                         thumb={siteConfig('LANDING_HERO_VIDEO_IMAGE', null, CONFIG)}
                         thumbWidth={768}
                         thumbHeight={432}
