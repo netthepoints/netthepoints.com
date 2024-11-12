ae23190fb8ad297abf07ad32911f0c64def44611
netthepoints <netthepoints@gmail.com>
2024-11-12 15:32:52 +0800

    Update Hero.js

--- themes/starter/components/Hero.js
+++ themes/starter/components/Hero.js
@@ -31,21 +31,21 @@ export default function Hero() {
                     {/* Section header */}
                     <div className="text-center pb-12 md:pb-16">
                         <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
-                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">{siteConfig('LANDING_HERO_TITLE_1', null, CONFIG)}</span>
+                            <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">{siteConfig('STARTER_HERO_TITLE_1', null, CONFIG)}</span>
                         </h1>
                         <div className="max-w-3xl mx-auto">
-                            <p className="text-xl text-gray-600 dark:text-gray-400 mb-8" data-aos="zoom-y-out" data-aos-delay="150">{siteConfig('LANDING_HERO_P_1', null, CONFIG)}</p>
+                            <p className="text-xl text-gray-600 dark:text-gray-400 mb-8" data-aos="zoom-y-out" data-aos-delay="150">{siteConfig('STARTER_HERO_P_1', null, CONFIG)}</p>
                             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center" data-aos="zoom-y-out" data-aos-delay="300">
                                 <div>
                                     <a target='_blank' className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0"
-                                        href={siteConfig('LANDING_HERO_BUTTON_1_LINK', null, CONFIG)} rel="noreferrer">
-                                        {siteConfig('LANDING_HERO_BUTTON_1_TEXT', null, CONFIG)}
+                                        href={siteConfig('STARTER_HERO_BUTTON_1_LINK', null, CONFIG)} rel="noreferrer">
+                                        {siteConfig('STARTER_HERO_BUTTON_1_TEXT', null, CONFIG)}
                                     </a>
                                 </div>
                                 <div>
                                     <a target='_blank' className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4"
-                                        href={siteConfig('LANDING_HERO_BUTTON_2_LINK', null, CONFIG)} rel="noreferrer">
-                                        {siteConfig('LANDING_HERO_BUTTON_2_TEXT', null, CONFIG)}
+                                        href={siteConfig('STARTER_HERO_BUTTON_2_LINK', null, CONFIG)} rel="noreferrer">
+                                        {siteConfig('STARTER_HERO_BUTTON_2_TEXT', null, CONFIG)}
                                     </a>
                                 </div>
                             </div>
@@ -54,7 +54,7 @@ export default function Hero() {
 
                     {/* Hero image */}
                     <ModalVideo
-                        thumb={siteConfig('LANDING_HERO_VIDEO_IMAGE', null, CONFIG)}
+                        thumb={siteConfig('STARTER_HERO_VIDEO_IMAGE', null, CONFIG)}
                         thumbWidth={768}
                         thumbHeight={432}
                         thumbAlt={siteConfig('HERO_HEADER_1', null, CONFIG)}
