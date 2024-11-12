3e19163d96d4888241572b74600716f022046956
netthepoints <netthepoints@gmail.com>
2024-11-12 15:19:17 +0800

    Update About.js

--- themes/starter/components/About.js
+++ themes/starter/components/About.js
@@ -52,6 +52,139 @@ export const About = () => {
                       className="h-full w-full object-cover object-center"
                       />
                   </div>
+
+                  <div
+                    className="relative z-10 mb-4 flex items-center justify-center overflow-hidden bg-primary px-6 py-12 sm:mb-8 sm:h-[160px] sm:p-5 lg:mb-4 xl:mb-8"
+                  >
+                    <div>
+                      <span className="block text-5xl font-extrabold text-white">
+                        {siteConfig('STARTER_ABOUT_TIPS_1', null, CONFIG)}
+                      </span>
+                      <span className="block text-base font-semibold text-white">
+                        {siteConfig('STARTER_ABOUT_TIPS_2', null, CONFIG)}
+                      </span>
+                      <span
+                        className="block text-base font-medium text-white text-opacity-70"
+                      >
+                        {siteConfig('STARTER_ABOUT_TIPS_3', null, CONFIG)}
+                      </span>
+                    </div>
+                    <div>
+                      <span className="absolute left-0 top-0 -z-10">
+                        <svg
+                          width="540"
+                          height="144"
+                          viewBox="0 0 540 144"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <rect
+                            opacity="0.1"
+                            x="-67"
+                            y="47.127"
+                            width="113.378"
+                            height="131.304"
+                            transform="rotate(-42.8643 -67 47.127)"
+                            fill="url(#paint0_linear_1416_214)"
+                          />
+                          <defs>
+                            <linearGradient
+                              id="paint0_linear_1416_214"
+                              x1="-10.3111"
+                              y1="47.127"
+                              x2="-10.3111"
+                              y2="178.431"
+                              gradientUnits="userSpaceOnUse"
+                            >
+                              <stop stopColor="white" />
+                              <stop
+                                offset="1"
+                                stopColor="white"
+                                stopOpacity="0"
+                              />
+                            </linearGradient>
+                          </defs>
+                        </svg>
+                      </span>
+                      <span className="absolute right-0 top-0 -z-10">
+                        <svg
+                          width="540"
+                          height="97"
+                          viewBox="0 0 540 97"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <rect
+                            opacity="0.1"
+                            x="0.86792"
+                            y="-6.67725"
+                            width="155.563"
+                            height="140.614"
+                            transform="rotate(-42.8643 0.86792 -6.67725)"
+                            fill="url(#paint0_linear_1416_215)"
+                          />
+                          <defs>
+                            <linearGradient
+                              id="paint0_linear_1416_215"
+                              x1="78.6495"
+                              y1="-6.67725"
+                              x2="78.6495"
+                              y2="133.937"
+                              gradientUnits="userSpaceOnUse"
+                            >
+                              <stop stopColor="white" />
+                              <stop
+                                offset="1"
+                                stopColor="white"
+                                stopOpacity="0"
+                              />
+                            </linearGradient>
+                          </defs>
+                        </svg>
+                      </span>
+                      <span className="absolute bottom-0 right-0 -z-10">
+                        <svg
+                          width="175"
+                          height="104"
+                          viewBox="0 0 175 104"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <rect
+                            opacity="0.1"
+                            x="175.011"
+                            y="108.611"
+                            width="101.246"
+                            height="148.179"
+                            transform="rotate(137.136 175.011 108.611)"
+                            fill="url(#paint0_linear_1416_216)"
+                          />
+                          <defs>
+                            <linearGradient
+                              id="paint0_linear_1416_216"
+                              x1="225.634"
+                              y1="108.611"
+                              x2="225.634"
+                              y2="256.79"
+                              gradientUnits="userSpaceOnUse"
+                            >
+                              <stop stopColor="white" />
+                              <stop
+                                offset="1"
+                                stopColor="white"
+                                stopOpacity="0"
+                              />
+                            </linearGradient>
+                          </defs>
+                        </svg>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     {/* <!-- ====== About Section End --> */}
